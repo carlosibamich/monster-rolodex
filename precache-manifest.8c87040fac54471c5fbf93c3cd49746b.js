7791ec9b96c694d516175bfa9d9a9106333ef507
Carlos Ibarra <carlosibarra.art@gmail.com>
2020-05-05 16:10:22 -0700

    Updates

---
+++ precache-manifest.8c87040fac54471c5fbf93c3cd49746b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a34622398d808859571b37a60e91db",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a25e68d2a4471e2d881e",
+    "url": "/monster-rolodex/static/css/main.4bc369e6.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25e68d2a4471e2d881e",
+    "url": "/monster-rolodex/static/js/main.cb01a0fa.chunk.js"
+  },
+  {
+    "revision": "1db901b49b16624dee18",
+    "url": "/monster-rolodex/static/js/runtime-main.b410029c.js"
+  }
+]);
